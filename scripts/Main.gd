0b6a6e79fbbe2fe1fd60cf33e07d30e57b6e11dd
Mitch Dart <mitch@sticitt.co.za>
2020-11-29 21:29:43 +0200

    Folder refactor

---
+++ scripts/Main.gd
@@ -0,0 +1,22 @@
+tool
+
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_wire()
+
+func set_wire():
+	var wire = get_node("Wire")
+	wire.start_point = get_node("AndGate").get_output_A_position()
+	wire.end_point = get_node("XNorGate").get_input_A_position()
+	wire.start_point_direction = get_node("AndGate").rotation_degrees
+	wire.end_point_direction = get_node("XNorGate").rotation_degrees
+
+
+func _on_component_drag():
+	set_wire()
---
+++ scripts/Wire.gd
@@ -0,0 +1,61 @@
+tool
+
+extends Node2D
+
+export var start_point = Vector2(0.0,0.0) setget set_startpoint
+export var end_point = Vector2(0.0,0.0) setget set_endpoint
+
+export var tension = 1.0 setget set_tension
+
+export var start_point_direction = 0.0 setget set_start_point_direction
+export var end_point_direction = 0.0 setget set_end_point_direction
+
+export var width = 10 
+export var border_width = 5 
+
+func set_startpoint(new_startpoint):
+	if start_point != new_startpoint:
+		start_point = new_startpoint
+		update()
+
+func set_endpoint(new_endpoint):
+	if end_point != new_endpoint:
+		end_point = new_endpoint
+		update()
+		
+func set_tension(new_tension):
+	if tension != new_tension:
+		tension = new_tension
+		update()
+		
+func set_start_point_direction(new_start_point_direction):
+	if start_point_direction != new_start_point_direction:
+		start_point_direction = new_start_point_direction
+		update()
+		
+func set_end_point_direction(new_end_point_direction):
+	if end_point_direction != new_end_point_direction:
+		end_point_direction = new_end_point_direction
+		update()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _draw():
+	var cp1 = Transform2D.IDENTITY
+	cp1 = cp1.rotated(deg2rad(start_point_direction)).translated(Vector2(0.0,tension))
+	var cp2 = Transform2D.IDENTITY
+	cp2 = cp2.rotated(deg2rad(end_point_direction)).translated(Vector2(0.0,-tension))
+	
+	var inA = Vector2(0.0,0.0)
+	var inB = cp2.xform(Vector2(0.0,0.0))
+	var outA = cp1.xform(Vector2(0.0,0.0))
+	var outB = Vector2(0.0,0.0)
+	
+	var curve = Curve2D.new()
+	curve.add_point(start_point, inA, outA)
+	curve.add_point(end_point, inB, outB)
+	var points = curve.tessellate()
+	draw_polyline(points, Color.black, width, true)
+	draw_polyline(points, Color.white, width - border_width, true)
