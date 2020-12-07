b531b1be8e0d987bc17ab61301d746a61dadc343
Mitch Dart <mitch@sticitt.co.za>
2020-12-07 23:04:48 +0200

    Added a switch animation POC

---
+++ scene/AnimatedSprite.gd
@@ -0,0 +1,27 @@
+extends AnimatedSprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+var on = false
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton && event.is_pressed():
+		if on:
+			play("off")
+		else:
+			play("on")
+		on = !on
